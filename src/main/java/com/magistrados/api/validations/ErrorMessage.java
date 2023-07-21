19b860b900d3566cf82fb375a9870348fa1b68a8
Leonardo da Silva <oproprioleonardo@gmail.com>
2023-07-21 20:23:00 -0300

    fix: disallow duplicate team

--- src/main/java/com/magistrados/api/validations/ErrorMessage.java
+++ src/main/java/com/magistrados/api/validations/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     NOT_NEGATIVE_NUMBER("O campo '{field}' não pode ser um número nulo ou negativo."),
     NOT_VALID_PLAYER_NUMBER("O campo '{field}' não é um número inteiro no intervalo de 1-100."),
     NOT_VALID_DATE("O campo '{field}' não possui uma data válida (dd/MM/yyyy)."),
-    NOT_VALID_TIME("O campo '{field}' não possui um horário válido (HH:mm).");
+    NOT_VALID_TIME("O campo '{field}' não possui um horário válido (HH:mm)."),
+    NOT_REPEATABLE("Os campos '{field}' estão idênticos.");
 
     private final String text;
 

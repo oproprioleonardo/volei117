ec2adf1ecd371d28d18c763268764976be9aec09
Gustavo <gpontesf06@gmail.com>
2023-07-23 14:58:53 -0300

    MatchViewer created. timer updates the match every 30 seconds

---
+++ src/main/java/com/magistrados/graph/screens/match/MatchViewer.java
@@ -0,0 +1,42 @@
+package com.magistrados.graph.screens.match;
+
+import com.magistrados.graph.notification.Notifications;
+import com.magistrados.models.Partida;
+import com.magistrados.services.PartidaService;
+import io.smallrye.mutiny.Uni;
+
+import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MatchViewer {
+    private PartidaService partidaService;
+    private Partida partida;
+    private Timer timer;
+
+    public MatchViewer(PartidaService partidaService, Partida partida) {
+        this.partidaService = partidaService;
+        this.partida = partida;
+        timer = new Timer();
+        timer.schedule(new updateMatch(),
+                30000, // delay inicial de 30seg
+                30000); // atualiza partida a cada 30seg
+    }
+
+    class updateMatch extends TimerTask {
+        @Override
+        public void run() {
+            Notifications.info("Salvando partida...");
+            final Uni<Void> emitter = Uni.createFrom().emitter((em) -> new Thread(() -> {
+                partidaService.salvarPartida(partida);
+                em.complete(null);
+            }).start());
+
+            emitter.subscribe().with(partida -> SwingUtilities.invokeLater(() -> {
+                Notifications.info("Partida salva");
+            }), failure -> {
+                Notifications.error("Erro ao salvar partida");
+            });
+        }
+    }
+}
